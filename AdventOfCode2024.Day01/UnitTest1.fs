062d8c09c19cf001073cf52767f3c70583876cb3
Vincent Rémond <vincent.remond@gmail.com>
2025-01-03 00:19:35 +0100

    quickstart

--- AdventOfCode2024.Day01/UnitTest1.fs
+++ AdventOfCode2024.Day01/UnitTest1.fs
@@ -1,59 +1,59 @@
 ﻿module AdventOfCode2024.Tests
-
-open AdventOfCode2024.Common
-open FluentAssertions
-open NUnit.Framework
-open FParsec
-open FSharp.Data.LiteralProviders
-
-[<RequireQualifiedAccess>]
-module Parser =
-  let parse =
-
-    let parseLine = (pint32 .>> spaces) .>>. pint32
-    runOrFail (sepBy parseLine newline)
-
-[<RequireQualifiedAccess>]
-module Solution =
-
-  let solve1 (data: (int * int) list) =
-    data
-    |> List.map Tuple2.toList
-    |> List.transpose
-    |> List.map List.sort
-    |> List.transpose
-    |> List.map ((List.reduce (-)) >> abs)
-    |> List.sum
-
-  let solve2 data =
-    let (first, second) = List.unzip data
-    let countPerValue = List.countBy id (second) |> Map.ofList
-
-    first
-    |> List.map (fun v ->
-      let count = countPerValue |> Map.tryFindOrDefault v 0
-      v * count
-
-    )
-    |> List.sum
-
-[<Test>]
-let Test1 () =
-
-  {
-    Parser = Parser.parse
-    Sample = TextFile.example.Text
-    UserData = TextFile.input.Text
-    Part1 = {
-      Solver = Solution.solve1
-      ForSample = 11
-      ForUserData = Some 2113135
-    }
-    Part2 =
-      Some {
-        Solver = Solution.solve2
-        ForSample = 31
-        ForUserData = Some 19097157
-      }
-  }
-    .run ()
+//
+// open FluentAssertions
+// open NUnit.Framework
+// open FParsec
+// open FSharp.Data.LiteralProviders
+// open Pinicola.FSharp
+//
+// [<RequireQualifiedAccess>]
+// module Parser =
+//     let parse =
+//
+//         let parseLine = (pint32 .>> spaces) .>>. pint32
+//         runOrFail (sepBy parseLine newline)
+//
+// [<RequireQualifiedAccess>]
+// module Solution =
+//
+//     let solve1 (data: (int * int) list) =
+//         data
+//         |> List.map Tuple2.toList
+//         |> List.transpose
+//         |> List.map List.sort
+//         |> List.transpose
+//         |> List.map ((List.reduce (-)) >> abs)
+//         |> List.sum
+//
+//     let solve2 data =
+//         let (first, second) = List.unzip data
+//         let countPerValue = List.countBy id (second) |> Map.ofList
+//
+//         first
+//         |> List.map (fun v ->
+//             let count = countPerValue |> Map.tryFindOrDefault v 0
+//             v * count
+//
+//         )
+//         |> List.sum
+//
+// [<Test>]
+// let Test1 () =
+//
+//     {
+//         Parser = Parser.parse
+//         Sample = TextFile.example.Text
+//         UserData = TextFile.input.Text
+//         Part1 = {
+//             Solver = Solution.solve1
+//             ForSample = 11
+//             ForUserData = Some 2113135
+//         }
+//         Part2 =
+//             Some {
+//                 Solver = Solution.solve2
+//                 ForSample = 31
+//                 ForUserData = Some 19097157
+//             }
+//     }
+//         .run ()
