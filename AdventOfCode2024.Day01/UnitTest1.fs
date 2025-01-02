217f272a91b596f3ed78fb7bc4f65660eeea09a4
Vincent Rémond <vincent.remond@gmail.com>
2025-01-02 12:19:49 +0100

    quickstart

--- AdventOfCode2024.Day01/UnitTest1.fs
+++ AdventOfCode2024.Day01/UnitTest1.fs
@@ -8,52 +8,52 @@ open FSharp.Data.LiteralProviders
 
 [<RequireQualifiedAccess>]
 module Parser =
-    let parse =
+  let parse =
 
-        let parseLine = (pint32 .>> spaces) .>>. pint32
-        runOrFail (sepBy parseLine newline)
+    let parseLine = (pint32 .>> spaces) .>>. pint32
+    runOrFail (sepBy parseLine newline)
 
 [<RequireQualifiedAccess>]
 module Solution =
 
-    let solve1 (data: (int * int) list) =
-        data
-        |> List.map Tuple2.toList
-        |> List.transpose
-        |> List.map List.sort
-        |> List.transpose
-        |> List.map ((List.reduce (-)) >> abs)
-        |> List.sum
+  let solve1 (data: (int * int) list) =
+    data
+    |> List.map Tuple2.toList
+    |> List.transpose
+    |> List.map List.sort
+    |> List.transpose
+    |> List.map ((List.reduce (-)) >> abs)
+    |> List.sum
 
-    let solve2 data =
-        let (first, second) = List.unzip data
-        let countPerValue = List.countBy id (second) |> Map.ofList
+  let solve2 data =
+    let (first, second) = List.unzip data
+    let countPerValue = List.countBy id (second) |> Map.ofList
 
-        first
-        |> List.map (fun v ->
-            let count = countPerValue |> Map.tryFindOrDefault v 0
-            v * count
+    first
+    |> List.map (fun v ->
+      let count = countPerValue |> Map.tryFindOrDefault v 0
+      v * count
 
-        )
-        |> List.sum
+    )
+    |> List.sum
 
 [<Test>]
 let Test1 () =
 
-    {
-        Parser = Parser.parse
-        Sample = TextFile.example.Text
-        UserData = TextFile.input.Text
-        Part1 = {
-            Solver = Solution.solve1
-            ForSample = 11
-            ForUserData = Some 2113135
-        }
-        Part2 =
-            Some {
-                Solver = Solution.solve2
-                ForSample = 31
-                ForUserData = Some 19097157
-            }
+  {
+    Parser = Parser.parse
+    Sample = TextFile.example.Text
+    UserData = TextFile.input.Text
+    Part1 = {
+      Solver = Solution.solve1
+      ForSample = 11
+      ForUserData = Some 2113135
     }
-        .run ()
+    Part2 =
+      Some {
+        Solver = Solution.solve2
+        ForSample = 31
+        ForUserData = Some 19097157
+      }
+  }
+    .run ()
