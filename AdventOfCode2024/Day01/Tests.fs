ecc397fa45b67ad0049d187671c3041d8d039843
Vincent Rémond <vincent.remond@gmail.com>
2025-01-04 14:04:56 +0100

    update suite à la mise à jour

---
+++ AdventOfCode2024/Day01/Tests.fs
@@ -0,0 +1,64 @@
+﻿module AdventOfCode2024.Day01
+
+open NUnit.Framework
+open FParsec
+open FSharp.Data.LiteralProviders
+open Pinicola.FSharp
+open Pinicola.FSharp.FParsec
+
+[<RequireQualifiedAccess>]
+module Parser =
+    let parse =
+
+        let parseLine = (pint32 .>> spaces) .>>. pint32
+        parseOrFail (sepBy parseLine newline)
+
+[<RequireQualifiedAccess>]
+module Solution =
+
+    let solve1 (data: (int * int) list) =
+        data
+        |> List.map Tuple2.toList
+        |> List.transpose
+        |> List.map List.sort
+        |> List.transpose
+        |> List.map ((List.reduce (-)) >> abs)
+        |> List.sum
+
+    let solve2 data =
+        let (first, second) = List.unzip data
+        let countPerValue = List.countBy id (second) |> Map.ofList
+
+        first
+        |> List.map (fun v ->
+            let count = countPerValue |> Map.tryFindOrDefault v 0
+            v * count
+
+        )
+        |> List.sum
+
+[<Test>]
+let Test1 () =
+
+    Runner.exec {
+        Day = 1
+        Parser = Parser.parse
+        Sample = TextFile.example.Text |> Some
+        UserData = TextFile.input.Text |> Some
+        Part1 =
+            Some {
+                Solver = Solution.solve1
+                Solutions = {
+                    ForSample = Some 11
+                    ForUserData = Some 2113135
+                }
+            }
+        Part2 =
+            Some {
+                Solver = Solution.solve2
+                Solutions = {
+                    ForSample = Some 31
+                    ForUserData = Some 19097157
+                }
+            }
+    }
---
+++ AdventOfCode2024/Runner.fs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2024
+
+open FsUnitTyped
+open FsToolkit.ErrorHandling
+open Pinicola.FSharp.SpectreConsole
+
+type Runner<'parsedData, 'result when 'result: equality> = {
+    Day: int
+    Parser: string -> 'parsedData
+    Sample: string option
+    UserData: string option
+    Part1: Part<'parsedData, 'result> option
+    Part2: Part<'parsedData, 'result> option
+} with
+
+    static member run name parser (part: Part<'parsedData, 'result> option) (stringData: string option) (getResult: Solutions<'result> -> 'result option) =
+        let x =
+            option {
+                let! part = part
+                let! expected = getResult part.Solutions
+                let! stringData = stringData
+                let parsedData = parser stringData
+                let result = part.Solver parsedData
+                return (result, expected)
+            }
+
+        match x with
+        | Some(result, expected) ->
+            result |> shouldEqual expected
+            AnsiConsole.markupLineInterpolated $"[green]{name} passed[/]"
+        | None -> AnsiConsole.markupLineInterpolated $"[red]{name} not run[/]"
+
+    static member exec runner =
+        let getSampleSolution = _.ForSample
+        let getUserDataSolution = _.ForUserData
+
+        Runner.run "part1 / sample" runner.Parser runner.Part1 runner.Sample getSampleSolution
+        Runner.run "part1 / user " runner.Parser runner.Part1 runner.UserData getUserDataSolution
+        Runner.run "part2 / sample" runner.Parser runner.Part2 runner.Sample getSampleSolution
+        Runner.run "part2 / user" runner.Parser runner.Part2 runner.UserData getUserDataSolution
+
+and Part<'parsedData, 'result> = {
+    Solver: 'parsedData -> 'result
+    Solutions: Solutions<'result>
+}
+
+and Solutions<'result> = {
+    ForSample: 'result option
+    ForUserData: 'result option
+}
